ed0627d9d749dcac7938877608bcff7246dd9148
Dean Shaff <dshaff001@gmail.com>
2014-06-23 22:43:13 -0400

    Finally getting the correct values for photon index

--- 1862014.py
+++ 1862014.py
@@ -10,8 +10,9 @@ import scipy.stats as stats
 #from astropy.constants.cgs import h #plancks constant in cgs
 from plotting_tools import Graphs
 #Physical Constants=====================
-h = float(6.62606885e-27) #planks constant
+h = float(6.62606885e-27) #planks constant ergs*s
 conversionKeV = float(624150934) #ergs to keV
+h_ = float(4.135667516e-18) #plancks constant kev*s
 #General Variables======================
 path_to_gelfand = '/home/dean/gelfand_pwn/2014-06-16'
 phot = 'modelres.photspec.fits'
@@ -44,9 +45,10 @@ def main():
 	datadyn1 = filedyn[1].data
 	time = datadyn1.field(0)
 	def photon_index():
-		p_2 = [float(2.5) for i in time]
-		avfreq = dataphot1.field(3)
-		avenerg = [float(i*h*conversionKeV) for i in avfreq] #converting into keV
+		p_2 = np.array([float(2.5) for i in time])
+		gamma_expected = np.array([float(3.5/2) for i in time])
+		avfreq = np.array(dataphot1.field(3))
+		avenerg = np.asarray([float(i*h*conversionKeV) for i in avfreq]) #converting into keV
 		t = 0
 		indices = []
 		k_indices = [] #finding indices around keV = 1
@@ -62,20 +64,26 @@ def main():
 		deviations = []
 		for i in xrange(0,len(dataphot2)):
 			luminosities = dataphot2[i]
-			number_photon = [float((10**40*k[0]/(conversionKeV*k[1]))) for k in zip(luminosities,avfreq)] #converting luminosity into number of photons
+			#number_photon = [float((10**40*k[0]/(conversionKeV*k[1]))) for k in zip(luminosities,avfreq)] #converting luminosity into number of photons
 			logdNdE = []
 			dNdE = []
 			ones = []
 			for j in indices:
-				differencephoton = float((number_photon[j+1] - number_photon[j]))
-				differenceenerg = float((avenerg[j+1] - avenerg[j]))
-				logdNdE.append(float(np.log(np.absolute(differencephoton/differenceenerg))))
-				dNdE.append(float(np.absolute(differencephoton/differenceenerg)))
+				luminositydensity = float((luminosities[j]*h_)/(h*avfreq[j]))
+				dNdE.append(luminositydensity) 
+				logdNdE.append(float(np.log(luminositydensity)))
+				# differencephoton = float((number_photon[j+1] - number_photon[j]))
+				# differenceenerg = float((avenerg[j+1] - avenerg[j]))
+				# logdNdE.append(float(np.log(np.absolute(differencephoton/differenceenerg))))
+				# dNdE.append(float(np.absolute(differencephoton/differenceenerg)))
 				ones.append(float(1))
-			k_actual = 	np.log((float(np.absolute(number_photon[int(k_indices[1])] - number_photon[int(k_indices[0])])))/(float(np.absolute(avenerg[int(k_indices[1])] - avenerg[int(k_indices[0])]))))
+			#k_actual = 	np.log((float(np.absolute(number_photon[int(k_indices[1])] - number_photon[int(k_indices[0])])))/(float(np.absolute(avenerg[int(k_indices[1])] - avenerg[int(k_indices[0])]))))
 			fit_matrix = np.array(zip(logavenerg,ones))
 			m,c = linalg.lstsq(fit_matrix,logdNdE)[0] #gives me the least square solution to A.x = b
 			m = np.absolute(m)
+			'''
+			Below I make some plots to check whether my stuff is working
+			'''
 			def power_law_plot(j):	
 				def f(t):
 					return t**(-m)
@@ -83,24 +91,24 @@ def main():
 					fig = plt.figure(figsize=(13,8))
 					ax = fig.add_subplot(111)
 					plt.plot(avenerg2to10,dNdE,'k.')
-					plt.plot(avenerg2to10,np.exp(k_actual)*f(avenerg2to10),'b')
+					plt.plot(avenerg2to10,np.exp(c)*f(avenerg2to10),'b')
 					plt.grid(True)
 					plt.show()
 			def linear_plot(j):
 				if i == int(j):
 					fig = plt.figure(figsize=(13,8))
 					ax = fig.add_subplot(111)
-					y = [float(k_actual-m*logavenerg[t]) for t in xrange(0,len(logavenerg))]
+					y = [float(c-m*logavenerg[t]) for t in xrange(0,len(logavenerg))]
 					plt.plot(logavenerg,y,'b')
 					plt.plot(logavenerg,logdNdE,'k.')
 					plt.grid(True)
 					plt.show()
-			# power_law_plot(50)
-			# linear_plot(50)
+			#power_law_plot(100)
+			#linear_plot(100)
 			# finding error in gamma:
 			errors = []
 			for j in xrange(0,len(logdNdE)):
-				gamma_actual = np.absolute(float((-logdNdE[j]+k_actual)/logavenerg[j]))
+				gamma_actual = np.absolute(float((-logdNdE[j]+c)/logavenerg[j]))
 				errors.append(np.absolute(float(gamma_actual-m))) #actual error term
 			deviations.append(float(np.std(errors)))
 			# resetting errors, logdNdE and ones
@@ -108,11 +116,11 @@ def main():
 			logdNdE = []
 			ones = []
 			photon_indices.append(np.absolute(m))
-		name = ['P_2']
-		y_data = [p_2]
+		name = ['P_2','Expected Gamma Value']
+		y_data = [p_2,gamma_expected]
 		photon_index_graph = Graphs('Photon Index vs Time in 2-10 keV range','Time (years)','Photon Indices',name,y_error_names='Photon Indices')
-		photon_index_graph.create_figure_logxory(time,y_data,y_lim=[2,5],xlog=True,error_ydata=photon_indices,y_error=deviations)
-	#photon_index()
+		photon_index_graph.create_figure_logxory(time,y_data,y_lim=[1,4],error_ydata=photon_indices,y_error=deviations)
+	photon_index()
 	def spectral_index():
 		 #Lumonisity (Flux) Density L_nu = C (nu / nu_0) ^ alpha
 		 # log(L_nu) = log(C) + alpha*(log(nu/nu_0))
@@ -128,20 +136,18 @@ def main():
 		 deviations = []
 		 for j in xrange(0,len(dataphot2)):
 		 	luminosities = dataphot2[j]
-		 	luminositiesradio = [luminosities[i] for i in indices_radio]
-		 	logluminosities = [np.log(luminosities[i]) for i in indices_radio]
+		 	luminositiesradio = np.asarray([luminosities[i] for i in indices_radio])
+		 	logluminosities = np.asarray([np.log(luminosities[i]) for i in indices_radio])
 		 	ones = [1 for i in indices_radio]
 		 	dummy = np.vstack((log_radio,ones))
 		 	fit_matrix = np.transpose(dummy)
 		 	m,c = linalg.lstsq(fit_matrix,logluminosities)[0]
 		 	#Checking======================
-		 	def f_2(t):
-		 		return c+(m*t)
 		 	def plot_log(t):
 		 		if int(j) == int(t):
 			 		fig = plt.figure(figsize=(14,8))
 			 		ax = fig.add_subplot(111)
-			 		plt.plot(log_radio,f_2(log_radio),'b')
+			 		plt.plot(log_radio,c+(m*log_radio),'b')
 			 		plt.plot(log_radio,logluminosities,'k.')
 			 		plt.grid(True)
 			 		plt.show()
@@ -169,7 +175,7 @@ def main():
 		 spectral_index_graph = Graphs('Spectral Index vs Times', 'Time (years)','Spectral Index (unitless)',y_error_names='Spectral Index')
 		 spectral_index_graph.create_figure_logxory(time,error_ydata=alphas,y_error=deviations)
 
-	spectral_index()
+	#spectral_index()
 	def luminosity2to10(): #there was a units problem. fixed 20-6-2014
 		avfreq = dataphot1.field(3)
 		avenerg = [float(i*h*conversionKeV) for i in avfreq] #converting into keV
